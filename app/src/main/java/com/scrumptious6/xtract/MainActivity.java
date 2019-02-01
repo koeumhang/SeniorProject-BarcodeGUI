b3d58ec9b34ce138542418f4676340e5370f870f
DylanPatel <patelo.dylan@gmail.com>
2019-02-01 13:02:13 -0800

    Updated barcode implementation

--- app/src/main/java/com/scrumptious6/xtract/MainActivity.java
+++ app/src/main/java/com/scrumptious6/xtract/MainActivity.java
@@ -11,11 +11,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
     private ImageButton scanButton;
+    private TextView barcodeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,5 +68,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void scanBarcode(View v) {
+        Intent intent = new Intent(MainActivity.this, BarcodeActivity.class);
+        startActivityForResult(intent, 0);
+    }
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode ==0) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra("barcode");
+                    barcodeResult.setText("Barcode value: " + barcode.displayValue);
+                }
+                else {
+                    barcodeResult.setText("No barcode found");
+                }
+            }
+        }
+        else {
+
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
