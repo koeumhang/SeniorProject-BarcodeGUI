084a41a0a79cb627fde33994d91a002d2af2b2a5
Jessie Vela <jessievela@csus.edu>
2019-02-15 18:31:17 -0800

    Permissions added and fixed layout

--- app/src/main/java/com/scrumptious6/xtract/MainActivity.java
+++ app/src/main/java/com/scrumptious6/xtract/MainActivity.java
@@ -72,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /*
+        This method will process the result of the scanned barcode.
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
@@ -92,127 +95,4 @@ public class MainActivity extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
-
-
-
-
-}
-
-
-
-
-/*
-
--------------------------- old code -------------------------------------------------------
-
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-
-import com.google.android.gms.common.api.CommonStatusCodes;
-import com.google.android.gms.vision.barcode.Barcode;
-
-
-
-
-
-
-
-
-
-public class MainActivity extends AppCompatActivity {
-
-
-
-
-
-    public void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted.
-            // Asking user for permission to use CAMERA.
-            ActivityCompat.requestPermissions(this,
-                    new String[] {Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
-        }
-
-        scanButton = (ImageButton) findViewById(R.id.scanButton);
-        scanButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent barcodeActivity = new Intent(MainActivity.this, BarcodeActivity.class);
-                startActivity(barcodeActivity);
-            }
-        });
-        // Checks to see if permission is given from user to use CAMERA.
-
-
-
-
-    }
-
-
-
-    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
-    private ImageButton scanButton;
-    private TextView barcodeResult;
-
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
-    }
-
-    public void scanBarcode(View v) {
-        Intent intent = new Intent(MainActivity.this, BarcodeActivity.class);
-        startActivityForResult(intent, 0);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (requestCode ==0) {
-            if (resultCode == CommonStatusCodes.SUCCESS) {
-                if (data != null) {
-                    Barcode barcode = data.getParcelableExtra("barcode");
-                    barcodeResult.setText("Barcode value: " + barcode.displayValue);
-                }
-                else {
-                    barcodeResult.setText("No barcode found");
-                }
-            }
-        }
-        else {
-
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-}
-*/
+} // End of MainActivity class
